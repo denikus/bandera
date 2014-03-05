fdf710aa282f39678a37a76815c578f756a11ccf
denix <denis.soloshenko@gmail.com>
2014-03-05 22:52:47 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bandera::Application.config.secret_key_base = '53b06987e1514f42321b2ca6ecff7c4ce1fa57058f201b237efa4504e51b7ec955779925710f5e86a7e6403d478d7c9c27bac4d60de09aa771247947e124edca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bandera::Application.config.session_store :cookie_store, key: '_bandera_session'
