24029c7f7963e194e7087c4e61c2a769664d333b
denix <denis.soloshenko@gmail.com>
2014-03-20 10:33:41 +0200

    fix migration

--- db/migrate/20140313080420_create_videos.rb
+++ db/migrate/20140313080420_create_videos.rb
@@ -1,10 +1,15 @@
 class CreateVideos < ActiveRecord::Migration
-  def change
-    create_table :videos do |t|
+  def up
+    create_table :videos, force: true do |t|
       t.belongs_to :user, :index => true
       t.string :url, :null => false
       t.string :hash_id, :null => false
       t.timestamps
     end
+    add_index :videos, :hash_id
+  end
+
+  def down
+    drop_table :videos
   end
 end
--- db/migrate/20140320081902_add_hash.rb
+++
@@ -1,5 +0,0 @@
-class AddHash < ActiveRecord::Migration
-  def change
-    add_index :videos, :hash_id
-  end
-end
