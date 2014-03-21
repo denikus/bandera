b3445dcc243e88b1afb7ee170fb327aa7f6e333f
denix <denis.soloshenko@gmail.com>
2014-03-21 17:27:33 +0200

    changes in facebook auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Bandera::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
-  devise_scope :user do
-    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
-    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
+  #devise_scope :user do
+  #  get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+  #  get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  #end
 
   root :to => "home#index"
   resources :videos
