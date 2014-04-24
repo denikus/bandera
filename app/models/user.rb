8e30932ae02dd933aedd59bbaaf517c4ebeae655
denix <denis.soloshenko@gmail.com>
2014-04-24 14:54:33 +0300

    remove ap

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ActiveRecord::Base
          :omniauthable, :omniauth_providers => [:facebook]
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
-    ap auth
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
 
     if user
