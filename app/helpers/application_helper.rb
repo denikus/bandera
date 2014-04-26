bf21d9c712fb855736499dcb19b0dc5adbf69931
denix <denis.soloshenko@gmail.com>
2014-04-26 16:02:12 +0300

    fix video, add location to user table

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def video_city(video)
+    return (video.city.blank? ? video.user.city : video.city)
+  end
+
+  def video_country(video)
+    return (video.country.blank? ? video.user.country : country.city)
+  end
 end
